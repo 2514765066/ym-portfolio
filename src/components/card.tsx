462061b352828d55e3dc731117234044b16e8c60
张铭洋 <2514765066@qq.com>
2026-08-06 15:05:20 +0800

    feat: 文章卡片新增动画效果

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,47 +1,81 @@
 import { Article } from '@/type';
 import { capitalize } from '@/utils/str';
 import { ArrowRight } from 'lucide-react';
+import { motion } from 'motion/react';
 import Link from 'next/link';
 
 type CardtProps = {
   data: Article;
 };
 
-//页面内容卡片
+// 页面内容卡片
 export const Card = ({ data }: CardtProps) => {
   return (
-    <Link
-      className='group w-full flex flex-col'
-      href={{
-        pathname: `/article/${data.id}`,
+    <motion.div
+      layout
+      initial={{
+        scale: 0,
+        opacity: 0.5,
+      }}
+      animate={{
+        scale: 1,
+        opacity: 1,
+      }}
+      exit={{
+        scale: 0,
+        opacity: 0.5,
+      }}
+      transition={{
+        type: 'spring',
+        stiffness: 300,
+        damping: 25,
+        duration: 400,
       }}
     >
-      <header className='mb-1.5 flex justify-between items-center'>
-        <span className='text-xs tracking-widest'>
-          - {capitalize(data.tag)} -
-        </span>
+      <Link
+        className='group w-full flex flex-col'
+        href={{
+          pathname: `/article/${data.id}`,
+        }}
+      >
+        <header className='mb-1.5 flex justify-between items-center'>
+          <span className='text-xs tracking-widest'>
+            - {capitalize(data.tag)} -
+          </span>
 
-        <span className='text-xs tracking-widest'>(010)</span>
-      </header>
+          <span className='text-xs tracking-widest'>(010)</span>
+        </header>
 
-      <main className='aspect-3/2 border rounded-md overflow-hidden bg-muted'>
-        <img
-          className='size-full object-cover hover:scale-105 transition-transform duration-400'
-          src={data.img}
-        />
-      </main>
+        <main className='aspect-3/2 border rounded-md overflow-hidden bg-muted'>
+          <img
+            className='size-full object-cover hover:scale-105 transition-transform duration-400'
+            src={data.img}
+          />
+        </main>
 
-      <footer className='mt-3 flex flex-col gap-1'>
-        <div className='flex items-center'>
-          <p className='text-lg truncate font-semibold'>{data.title}</p>
+        <footer className='mt-3 flex flex-col gap-1'>
+          <div className='flex items-center'>
+            <p className='text-lg truncate font-semibold'>{data.title}</p>
 
-          <ArrowRight className='ml-auto shrink-0 text-muted-foreground -rotate-45 stroke-[1.5px] group-hover:rotate-0 transition-transform duration-400' />
-        </div>
+            <ArrowRight
+              className='
+                ml-auto 
+                shrink-0 
+                text-muted-foreground 
+                -rotate-45 
+                stroke-[1.5px] 
+                group-hover:rotate-0 
+                transition-transform 
+                duration-400
+              '
+            />
+          </div>
 
-        <span className='text-sm text-muted-foreground truncate'>
-          {data.description}
-        </span>
-      </footer>
-    </Link>
+          <span className='text-sm text-muted-foreground truncate'>
+            {data.description}
+          </span>
+        </footer>
+      </Link>
+    </motion.div>
   );
 };
