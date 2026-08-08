2aa430b053cef4998844f839c4e64bcc558cc34f
张铭洋 <2514765066@qq.com>
2026-08-08 11:11:17 +0800

    feat: 新增标签页标题

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import { interFont, geistFont, geistMonoFont, hwmcFont } from '@/font/index';
 import { cn } from '@/lib/utils';
 import '@/style/globals.css';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '张铭洋的个人作品集',
+};
 
 export default function RootLayout({
   children,
