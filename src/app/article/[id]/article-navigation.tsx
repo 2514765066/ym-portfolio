6f33b143537ba4fab2706acd70866f085355f398
张铭洋 <2514765066@qq.com>
2026-08-09 11:26:10 +0800

    feat: 新增文章页上一篇下一篇对移动端适配

--- src/app/article/[id]/article-navigation.tsx
+++ src/app/article/[id]/article-navigation.tsx
@@ -23,7 +23,7 @@ export const ArticleNavigation = ({
   }
 
   return (
-    <nav className='flex gap-4'>
+    <nav className='flex gap-4 max-sm:flex-col'>
       {previousArticle && (
         <Item
           className='flex-1 px-4.5 py-4 hover:-translate-y-1 transition-all! duration-300'
